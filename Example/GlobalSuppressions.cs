c40d11d6c23a1af3bd25797087d069645ca490d4
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2024-02-28 21:59:19 +0100

    Add source

---
+++ Example/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods",
+    Justification = "Allow auto-generated migrations to pass unmodified.", Scope = "namespaceanddescendants", Target = "~N:Example.Migrations")]
---
+++ SqliteTimestamp/SqliteTimestampMigrationsSqlGenerator.cs
@@ -0,0 +1,81 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dorssel.EntityFrameworkCore.Migrations;
+
+public class SqliteTimestampMigrationsSqlGenerator(MigrationsSqlGeneratorDependencies dependencies, IRelationalAnnotationProvider relationalAnnotationProvider)
+    : SqliteMigrationsSqlGenerator(dependencies, relationalAnnotationProvider)
+{
+    sealed record TableSchema(string TableName, string? Schema);
+
+    public override IReadOnlyList<MigrationCommand> Generate(IReadOnlyList<MigrationOperation> operations, IModel? model, MigrationsSqlGenerationOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(operations);
+
+        if (model is null)
+        {
+            return base.Generate(operations, model, options);
+        }
+
+        var tablesAffected = new HashSet<TableSchema>();
+
+        var augmentedOperations = new List<MigrationOperation>(operations);
+        foreach (var operation in operations)
+        {
+            switch (operation)
+            {
+                case TableOperation tableOperation:
+                    if (operation is RenameTableOperation renameTableOperation)
+                    {
+                        tablesAffected.Add(new(renameTableOperation.NewName ?? renameTableOperation.Name, renameTableOperation.NewSchema));
+                    }
+                    tablesAffected.Add(new(tableOperation.Name, tableOperation.Schema));
+                    break;
+
+                case ColumnOperation columnOperation:
+                    tablesAffected.Add(new(columnOperation.Table, columnOperation.Schema));
+                    break;
+            }
+        }
+
+        foreach (var affected in tablesAffected)
+        {
+            var delimitedTriggerName = Dependencies.SqlGenerationHelper.DelimitIdentifier(affected.TableName + "_TimestampUpdater", affected.Schema);
+            augmentedOperations.Add(new SqlOperation()
+            {
+                Sql = $"DROP TRIGGER IF EXISTS {delimitedTriggerName};"
+            });
+            if (model.GetEntityTypes().SingleOrDefault(et => et.GetTableName() == affected.TableName && et.GetSchema() == affected.Schema) is not IEntityType entityType)
+            {
+                // Table not found; assume it was dropped.
+                continue;
+            }
+            if (entityType.GetProperties().SingleOrDefault(p => p.IsConcurrencyToken && p.ValueGenerated == ValueGenerated.OnAddOrUpdate) is not IProperty property)
+            {
+                // No [Timestamp] column found.
+                continue;
+            }
+            var delimitedTableName = Dependencies.SqlGenerationHelper.DelimitIdentifier(affected.TableName);
+            var delimitedColumnName = Dependencies.SqlGenerationHelper.DelimitIdentifier(property.Name);
+            augmentedOperations.Add(new SqlOperation()
+            {
+                Sql = $"""
+                    CREATE TRIGGER {delimitedTriggerName} AFTER UPDATE ON {delimitedTableName}
+                    BEGIN
+                        UPDATE {delimitedTableName}
+                        SET {delimitedColumnName} = {delimitedColumnName} + 1
+                        WHERE rowid = NEW.rowid;
+                    END;
+                    """
+            });
+        }
+
+        return base.Generate(augmentedOperations, model, options);
+    }
+}
