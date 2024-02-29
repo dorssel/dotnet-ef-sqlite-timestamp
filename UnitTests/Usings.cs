55c824be380eea215a664a9d7b70159c3faebc19
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2024-02-29 02:19:24 +0100

    Add unit testing

---
+++ UnitTests/Usings.cs
@@ -0,0 +1,7 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+global using Dorssel.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
