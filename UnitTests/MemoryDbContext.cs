057b789ad9421a04c25cbfb1ede60d4cfbcc6e4e
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2024-03-01 13:18:49 +0100

    Add more tests

---
+++ UnitTests/MemoryDbContext.cs
@@ -0,0 +1,34 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+namespace UnitTests;
+
+sealed class MemoryDbContext : DbContext
+{
+    public const string TestTableName = "TestTable";
+    public const string IdName = "Id";
+    public const string RowVersionName = "RowVersion";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlite("DataSource=:memory:")
+            .UseSqliteTimestamp()
+            .LogTo(Console.WriteLine);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity(TestTableName, b =>
+        {
+            b.Property<long>(IdName)
+                    .ValueGeneratedOnAdd();
+
+            b.Property<byte[]>(RowVersionName)
+                    .IsRowVersion();
+
+            b.HasKey(IdName);
+        });
+    }
+}
